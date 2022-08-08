f560d73aa2abdad4e2bcd4e8cb800a2d6e0b8748
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2022-08-08 16:56:08 +0300

    initial

---
+++ app/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
+    category = models.ForeignKey('Category', null=True, on_delete=models.PROTECT, verbose_name='Категория')
+    title = models.CharField(max_length=50, verbose_name='Публикация')
+    body = models.TextField(blank=True, verbose_name='Контент')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Публикация'
+        verbose_name_plural = 'Публикации'
+        ordering = ['-created_at']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Наименование категории')
+
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
