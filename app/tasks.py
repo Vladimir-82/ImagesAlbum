456ae98bb27b54aa0f961626d01562ae2c9f4e41
vladimir <grun_gespenst@tut.by>
2022-08-16 23:11:53 +0300

    task_moded

--- app/tasks.py
+++ app/tasks.py
@@ -7,14 +7,12 @@ from .models import Post
 @app.task
 def send_view_count_report():
     '''Sends a message to the top 3 users with the most views'''
-    users = []
     queryset = Post.objects.order_by('-views', 'created_at')[:3]
-    for user in queryset:
-        users.append(user.author.email)
-        send_mail(
-            'You are in the top 3 of our album!',
-            'Congratulations! You are in the top 3 of our album Keep it up!',
-            'from@sever_album.com',
-            users,
-            fail_silently=False,
-        )
+    users = [user.author.email for user in queryset]
+    send_mail(
+        'You are in the top 3 of our album!',
+        'Congratulations! You are in the top 3 of our album Keep it up!',
+        'from@sever_album.com',
+        users,
+        fail_silently=False,
+    )
