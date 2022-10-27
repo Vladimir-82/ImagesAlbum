39305008ce25cd9ef21b85e85f40d64b5bda3a38
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2022-10-27 15:00:51 +0300

    tests added

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
 
-# Create your tests here.
+
+class View(TestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/api/v1/')
+        self.assertEqual(response.status_code, 200)
+
+
+class BlogTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        testuser1 = User.objects.create_user(
+        username='testuser1', password='abc123')
+        test_post = Post.objects.create(
+        author=testuser1, title='Blog title', body='Body content...')
+
+
+    def test_blog_content(self):
+        post = Post.objects.get(id=1)
+        author = f'{post.author}'
+        title = f'{post.title}'
+        body = f'{post.body}'
+        self.assertEqual(author, 'testuser1')
+        self.assertEqual(title, 'Blog title')
+        self.assertEqual(body, 'Body content...')
+
